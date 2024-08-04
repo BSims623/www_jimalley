e5f44da475516633de372c6f36b1350b3f1419c8
Brian Sims <briansims623@gmail.com>
2024-08-04 15:50:59 -0700

    Initial Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname);
+
+const nextConfig = {
+    output: 'export',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+};
 
 export default nextConfig;
