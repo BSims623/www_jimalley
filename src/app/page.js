c64da2e73f047ff39ba1821a71df26c945ef0b2c
Brian Sims <briansims623@gmail.com>
2024-08-05 10:29:22 -0700

    Added Global and Home Styling.

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,13 @@ export default function Home() {
           <img src={"/images/landing/landing_image.jpg"} className={`${styles.landing_image} ${styles.desktop_image}`} /> 
         </div>
         <div className={styles.content_container}>
-          <h1 className={styles.title}>Jim Alley</h1>
-          <h2 className={styles.subtitle}>Creative Writer</h2>
+          <div className={styles.title_container}>
+            <h1 className={styles.title}>Jim Alley</h1>
+            <h2 className={styles.subtitle}>Creative Writer</h2>
+          </div>
           <img src={"/images/landing/landing_image.jpg"} className={`${styles.landing_image} ${styles.mobile_image}`} />
           <p className={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. 
           </p>
           <Link href="/about" className="btn landing_btn">Read More</Link>
         </div>
