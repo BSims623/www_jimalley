1d12460cf5393035c9b485c024aa4cf98bf7c217
Brian Sims <briansims623@gmail.com>
2024-08-07 10:25:45 -0700

    Tickets to Salvation page

---
+++ src/app/tickets-to-salvation/sample/page.js
@@ -0,0 +1,10 @@
+export default function Sample() {
+    return (
+        <main>
+            <div>
+                <h1>Sample</h1>
+                <p>Sample content</p>
+            </div>
+        </main>
+    )
+}
