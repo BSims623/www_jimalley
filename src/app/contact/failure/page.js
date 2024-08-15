d51bdfa50a59ea143d4af9c2224ea8a8405f7091
Brian Sims <briansims623@gmail.com>
2024-08-14 21:32:40 -0700

    Email error page styling

--- src/app/contact/failure/page.js
+++ src/app/contact/failure/page.js
@@ -4,7 +4,7 @@ import styles from "@/styles/pages/contactStatus.module.scss"
 export default function EmailError() {
     return (
         <main className={styles.main}>
-            <h1>There was an error sending your message try again later.</h1>
+            <h1 className={styles.message}>There was an error sending your message try again later.</h1>
             <Link className="btn" href="/">Back Home</Link>
         </main>
     )
