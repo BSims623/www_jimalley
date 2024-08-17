408866518f5c86bdb4b75a7253699c788305aac4
Brian Sims <briansims623@gmail.com>
2024-08-17 16:32:44 -0700

    Added alt to images.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,12 +10,12 @@ export default function About() {
           <p className={styles.content_paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
           </p>
           <div className={`img_container ${styles.img_container} ${styles.about_image_right}`}>
-            <ExportedImage src={"/images/contact/contact_image.jpg"} fill={true} />
+            <ExportedImage src={"/images/contact/contact_image.jpg"} fill={true} alt="About image." />
           </div> 
         </section>
         <section className={`${styles.content_section} ${styles.content_section_left_image}`}>
           <div className={`img_container ${styles.img_container}`}>
-            <ExportedImage src={"/images/about/about_image_2.jpg"} fill={true} />
+            <ExportedImage src={"/images/about/about_image_2.jpg"} fill={true} alt="About image." />
           </div>
           <p className={''}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
           </p>
@@ -24,12 +24,12 @@ export default function About() {
           <p className={styles.content_paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
           </p>
           <div className={`img_container ${styles.img_container} ${styles.about_image_right}`}>
-            <ExportedImage src={"/images/about/about_image_3.jpg"} fill={true} />
+            <ExportedImage src={"/images/about/about_image_3.jpg"} fill={true} alt="About image." />
           </div>
         </section>
         <section className={`${styles.content_section} ${styles.content_section_left_image}`}>
           <div className={`img_container ${styles.img_container}`}>
-            <ExportedImage src={"/images/about/about_image_4.jpg"} fill={true} />
+            <ExportedImage src={"/images/about/about_image_4.jpg"} fill={true} alt="About image." />
           </div>
           <p className={''}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
           </p>
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <section className={styles.hero}>
         <div className={"img_container"}>
-          <ExportedImage src={"/images/landing/landing_image.jpg"} className={`${styles.landing_image} ${styles.desktop_image}`} fill={true} alt="Landing image."/> 
+          <ExportedImage className={`${styles.landing_image} ${styles.desktop_image}`} src={"/images/landing/landing_image.jpg"} fill={true} alt="Landing image."/> 
         </div>
         <div className={styles.content_container}>
           <div className={styles.title_container}>
@@ -15,7 +15,7 @@ export default function Home() {
             <h2 className={styles.subtitle}>Creative Writer</h2>
           </div>
           <div className={`${styles.mobile_img_container} img_container`}>
-            <ExportedImage src={"/images/landing/landing_image.jpg"} className={`${styles.landing_image} ${styles.mobile_image}`} fill={true} alt="Landing image."/>
+            <ExportedImage className={`${styles.landing_image} ${styles.mobile_image}`} src={"/images/landing/landing_image.jpg"} fill={true} alt="Landing image."/>
           </div>
           <p className={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. 
