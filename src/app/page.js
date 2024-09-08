eba90896cfc2691ff1949653022e8be39f0b680b
Brian Sims <briansims623@gmail.com>
2024-09-08 11:28:32 -0700

    Fixed escaped single quotes on home page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.title_container}>
-        <h1 className={styles.title}>WELCOME TO<br className={styles.mobile_break}></br> JIM ALLEY'S WEBSITE!!</h1>
+        <h1 className={styles.title}>WELCOME TO<br className={styles.mobile_break}></br> JIM ALLEY&apos;S WEBSITE!!</h1>
       </div>
       <section className={styles.hero}>
         <div className={"img_container"}>
@@ -18,18 +18,18 @@ export default function Home() {
             <ExportedImage className={`${styles.landing_image} ${styles.mobile_image}`} src={"/images/landing/landing_image.jpg"} fill={true} alt="Landing image."/>
           </div>
           <div className={styles.introduction_container}>
-            <p className={styles.paragraph}>I think at some point, everyone wants to write their story. Who doesn't want to capture a
+            <p className={styles.paragraph}>I think at some point, everyone wants to write their story. Who doesn&apos;t want to capture a
             remarkable memory, the sum of their life experiences, or the magic of the most intriguing
-            people they've known? For me, the passion to write came early, around age nine or ten,
+            people they&apos;ve known? For me, the passion to write came early, around age nine or ten,
             when I wrote several simple childhood stories about football heroes or insightful young
             people, handwritten on plain blank pages.</p>
-            <p className={styles.paragraph}>Now, a lifetime later, I'm a veteran West Sacramento, California writer. My short stories
+            <p className={styles.paragraph}>Now, a lifetime later, I&apos;m a veteran West Sacramento, California writer. My short stories
               have appeared in several national magazines and local literary magazines; my essays in
               an on-line journal, and my debut novel, Tickets to Salvation, is in final editing and will
               soon be available to the public.</p>
             <p>My primary inspiration to write has always been a stable of colorful friends, family, and
               acquaintances who have filled my life with joy, wonder, and intrigue. And, like all
-              writers, I've drawn from my life experiences, especially jobs from stable boy, gardener,
+              writers, I&apos;ve drawn from my life experiences, especially jobs from stable boy, gardener,
               and construction laborer to teacher, welfare worker, and government analyst. But maybe
               the most seductive draw is a belief that there a is purpose to life, something greater than
               us, a reason, a rationale, a symmetry that in the end makes life gratifying and complete.</p>
