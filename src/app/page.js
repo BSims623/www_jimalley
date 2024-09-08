4c02af65d3c733232c0e3ea13c3a6ae61d7b18d4
Brian Sims <briansims623@gmail.com>
2024-09-08 11:20:23 -0700

    Added verbage to home page along with styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,42 @@
 import styles from "@/styles/pages/home.module.scss";
 import ExportedImage from "next-image-export-optimizer";
 import Link from "next/link";
-import { countPages } from "@/utils/paginate.mjs";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>WELCOME TO<br className={styles.mobile_break}></br> JIM ALLEY'S WEBSITE!!</h1>
+      </div>
       <section className={styles.hero}>
         <div className={"img_container"}>
           <ExportedImage className={`${styles.landing_image} ${styles.desktop_image}`} src={"/images/landing/landing_image.jpg"} fill={true} alt="Landing image."/> 
         </div>
         <div className={styles.content_container}>
-          <div className={styles.title_container}>
-            <h1 className={styles.title}>Jim Alley</h1>
-            <h2 className={styles.subtitle}>Creative Writer</h2>
-          </div>
           <div className={`${styles.mobile_img_container} img_container`}>
             <ExportedImage className={`${styles.landing_image} ${styles.mobile_image}`} src={"/images/landing/landing_image.jpg"} fill={true} alt="Landing image."/>
           </div>
-          <p className={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. 
-          </p>
+          <div className={styles.introduction_container}>
+            <p className={styles.paragraph}>I think at some point, everyone wants to write their story. Who doesn't want to capture a
+            remarkable memory, the sum of their life experiences, or the magic of the most intriguing
+            people they've known? For me, the passion to write came early, around age nine or ten,
+            when I wrote several simple childhood stories about football heroes or insightful young
+            people, handwritten on plain blank pages.</p>
+            <p className={styles.paragraph}>Now, a lifetime later, I'm a veteran West Sacramento, California writer. My short stories
+              have appeared in several national magazines and local literary magazines; my essays in
+              an on-line journal, and my debut novel, Tickets to Salvation, is in final editing and will
+              soon be available to the public.</p>
+            <p>My primary inspiration to write has always been a stable of colorful friends, family, and
+              acquaintances who have filled my life with joy, wonder, and intrigue. And, like all
+              writers, I've drawn from my life experiences, especially jobs from stable boy, gardener,
+              and construction laborer to teacher, welfare worker, and government analyst. But maybe
+              the most seductive draw is a belief that there a is purpose to life, something greater than
+              us, a reason, a rationale, a symmetry that in the end makes life gratifying and complete.</p>
+            <p>Please learn more about <Link className={styles.link} href="/about">me</Link>, read an excerpt from my upcoming novel, <Link className={styles.link} href="tickets-to-salvation">Tickets to
+              Salvation</Link>, or read several of my <Link className={styles.link} href="short-stories">short stories</Link> on this site. 
+            </p>
+          </div>
           <Link href="/about"><button className="btn">Read More</button></Link>
         </div>
       </section>
