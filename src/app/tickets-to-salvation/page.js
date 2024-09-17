dbcd045077dd3dcb4dd775e2b7ad145dd80b0962
Brian Sims <briansims623@gmail.com>
2024-09-17 11:01:36 -0700

    Added revised verbiage to Tickets to Salvation page

--- src/app/tickets-to-salvation/page.js
+++ src/app/tickets-to-salvation/page.js
@@ -7,38 +7,42 @@ export default function TicketsToSalvation() {
       <div className={styles.content_container}>
         <h1>Tickets to Salvation</h1>
         <div className="paragraph_container">
-          <p>My debut novel, <em>Tickets to Salvation</em>, stems from my experiences as a Spanish&ndash;speaking
-            welfare worker in Santa Barbara, California. The novel careens through the traps and
-            pitfalls of the welfare system, the tyranny of living with a mentally disturbed parent, and
-            the efforts of a naïve young welfare worker to make a name for himself by rescuing an at&ndash;risk
-            child on his caseload.
+          <p>Fourteen-year-old Vita Pareto is about to achieve a life-long dream by performing as a
+            flamenco dancer in Santa Barbara&apos;s annual historical parade, <em>El Desfíle Histórico</em>. But as
+            she is climbing victoriously aboard a float before the parade, she is abruptly yanked to the
+            ground by her mother, Simpática, then before stunned onlookers, led back to their bleak
+            apartment building where Simpática burns Vita&apos;s flamenco dress and cuts up her dancing
+            shoes. Such outbursts are nothing new from this troubled and unstable mother, but this
+            one hurt, confirming what Vita has always suspected: happy endings were never meant
+            for her.
           </p>
-          <p>The story was inspired by real people and experiences I observed during my tenure at the
-            county welfare department. The first draft was sprawling &mdash; 1,100 pages &mdash; with multiple
-            primary characters and story lines. I submitted it to several publishing houses, and though
-            one major house asked to see more of the text, in the end, there were no takers and the
-            manuscript was tucked away in a dusty box in the garage where it sat for many years. But
-            I was haunted by that manuscript and convinced there was a good story lurking within.
+          <p>The family&apos;s welfare worker Mike Stanning has been scrambling to rescue Vita for a long
+            time with little success. Then something amazing happens. Vita quietly gathers her
+            courage and embarks on a magical journey to re-define herself and resurrect her dreams.
+            As she does, Mike&apos;s life spirals out of control, but soon the two will be drawn together,
+            consolidate their efforts, and look inward for courage and direction.
           </p>
-          <p>When I retired several years ago and resumed writing, I at first wrote about fifty short
-            stories over seven years. Then impulsively I decided to pull out that old yellowing
-            manuscript and transcribe it from hardcopy to electronic. A year later, I began editing. I
+          <p><em>Tickets to Salvation</em> stems from my experiences as a Spanish-speaking welfare worker in
+            Santa Barbara, California. The novel careens through the traps and pitfalls of the welfare
+            system, the tyranny of living with a mentally disturbed parent, and the efforts of a naïve
+            young welfare worker to make a name for himself by rescuing an at-risk child.
+          </p>
+          <p>The original draft was sprawling, 1,100 pages, with too many primary characters and
+            story lines. I submitted it to several publishing houses, and though one major house asked
+            to see more, there were in the end no takers and the manuscript was tucked away in a
+            dusty box in the garage for many years. But I was haunted by that manuscript and
+            convinced there was a good story lurking within.
+          </p>
+          <p>When I was able to retire several years ago, I wrote about fifty short stories and many
+            essays over seven years. Then impulsively I decided to pull out that old yellowing
+            manuscript and transcribe it from hardcopy to electronic. A year later, I began editing and
             came up with a good solid draft, but it was still 500 pages. After a professional editorial
             assessment, I revised it again, this time boiling it down to 280 pages. And now I think I
-            have an interesting and fast&ndash;moving story about a fourteen&ndash;year&ndash;old girl living with an
-            abusive single mother and showing remarkable fortitude and courage as she wends her
-            way from under her mother&apos;s yoke and through an unlikely journey to reach the dreams
-            she thought were unreachable.
-          </p>
-          <p>The story is rich in detail and steeped in Santa Barbara&apos;s history, local color and Mexican
-            heritage. It explores the welfare system, the protagonist&apos;s complex family history, and the
-            origins of flamenco. It is literary (and contemporary, and coming&ndash;of&ndash;age, and
-            bildungsroman) fiction, character&ndash;driven and 74,000&ndash;words. It is currently in the final
-            editing stage. Once available to the public, I will provide more specifics on this site.
-            Meanwhile, please enjoy a sampling of the first three chapters.
+            have a fast-moving and inspirational story about a disadvantaged young girl who pursues
+            an unlikely journey to reach heights she thought were unreachable.
           </p>
         </div>
-        <Link href="/tickets-to-salvation/chapter-1/page-1"><button className="btn">Read Sample</button></Link>
+        <Link href="/tickets-to-salvation/chapter-1/page-1"><button className={`btn ${styles.btn}`}>tickets to salvation</button></Link>
       </div>
     </main>
   );
