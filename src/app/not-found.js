5d616412e74259b91aba497914f4ef52336960d0
Brian Sims <briansims623@gmail.com>
2024-09-25 08:51:11 -0700

    Fixed not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,12 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from '@/styles/pages/notFound.module.scss';
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+    <main className={styles.main}>
+      <h1>Not Found</h1>
+      <p>This page does not exist...</p>
       <Link href="/"><button className='btn'>Back Home</button></Link>
-    </div>
+    </main>
   )
 }
